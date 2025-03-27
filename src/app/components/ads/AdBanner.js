eac0f7cd14be0dfba3e30a537659b7290d8c364b
Noman Mujahid <nomanmujahid.cs@gmail.com>
2025-03-27 22:53:29 +0500

    add: add AdSense

--- src/app/components/ads/AdBanner.js
+++ src/app/components/ads/AdBanner.js
@@ -7,11 +7,33 @@ import React, { useEffect } from "react";
 const AdBanner = ({ dataAdSlot, dataAdFormat, dataFullWidthResponsive }) => {
 
     useEffect(() => {
-        try {
-            (window.adsbygoogle = window.adsbygoogle || []).push({});
-        } catch (err) {
-            console.error(err);
+
+        const scriptElement = document.querySelector(
+            `script[src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8108715818808220"]`
+        )
+
+        const handleScriptLoad = () => {
+            try {
+                if(window.adsbygoogle){
+                    console.log('Pushing Ads')
+                    adsbygoogle.push({});
+                }else{
+                    scriptElement.addEventListener('load', handleScriptLoad)
+                    console.log('Waiting until adsense lib is loaded')
+                }
+            } catch (err) {
+                console.error("Err in Adsense " + err);
+            }
         }
+
+        handleScriptLoad();
+
+        return() => {
+            if(scriptElement){
+                scriptElement.removeEventListener("load", handleScriptLoad);
+            }
+        }
+
     }, []);
 
     return (
@@ -20,9 +42,9 @@ const AdBanner = ({ dataAdSlot, dataAdFormat, dataFullWidthResponsive }) => {
                 className="adsbygoogle"
                 style={{ display: "block" }}
                 data-ad-client="ca-pub-8108715818808220"
-                data-ad-slot={dataAdSlot}
-                data-ad-format={dataAdFormat}
-                data-full-width-responsive={dataFullWidthResponsive.toString()}
+                data-ad-slot={'7915411789'}
+                data-ad-format={'auto'}
+                data-full-width-responsive={'true'}
             ></ins>
         </>
     );
