0cef2b8fb78006e5a712d7538815c8a8aa7f084e
Noman Mujahid <nomanmujahid.cs@gmail.com>
2025-03-27 18:00:25 +0500

    add: testing script

--- src/app/components/ads/AdSense.js
+++ src/app/components/ads/AdSense.js
@@ -4,10 +4,10 @@ import React from 'react';
 const AdSense = ({ pId }) => {
     return (
         <Script
-            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}`}
-            crossOrigin='anonymous'
-            strategy='afterInteractive'
-        />
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8108715818808220"
+            crossOrigin="anonymous">
+        </Script>
     )
 }
 
